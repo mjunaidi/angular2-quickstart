25d73d34b350f381cabd1adfd05f3444e4900024
junaidi.jusoh@daimler.com <junaidi.jusoh@daimler.com>
2017-02-03 09:08:59 +0800

    Updated install.cmd

--- install.cmd
+++ install.cmd
@@ -1,9 +1,19 @@
-call git clone https://github.com/angular/quickstart.git quickstart
-cd quickstart
+set APP_NAME=angular2-app-test
+set GITHUB_USERID=mjunaidi
+
+call git clone https://github.com/angular/quickstart.git %APP_NAME%
+cd %APP_NAME%
 
 for /f %i in (non-essential-files.txt) do del %i /F /S /Q
 rd .git /s /q
 rd e2e /s /q
+
+git init
+git add .
+git commit -m "Initial commit"
+
+git remote add origin https://github.com/%GITHUB_USERID%/%APP_NAME%.git
+git push -u origin master
 
 call npm install
 call npm start
